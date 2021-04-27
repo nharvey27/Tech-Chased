6678f8ac950f2ce9f18f71036eab091244242de1
Nicholas Harvey <nharvey278@gmail.com>
2021-04-27 12:21:01 -0700

    apollo server config, run create next app for frontend side

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,9 +12,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_express_1 = require("apollo-server-express");
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const type_graphql_1 = require("type-graphql");
+const typeorm_1 = require("typeorm");
+const User_1 = require("./entities/User");
+const user_1 = require("./resolvers/user");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield typeorm_1.createConnection({
+        type: "postgres",
+        database: "techchased",
+        logging: true,
+        synchronize: true,
+        entities: [User_1.User],
+    });
     const app = express_1.default();
+    app.use(cors_1.default({
+        origin: "http://localhost:3000",
+        credentials: true,
+    }));
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [user_1.UserResolver],
+            validate: false,
+        }),
+    });
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
     app.listen(4000, () => {
         console.log("server started on  localhost:4000");
     });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,45 @@
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
 import express from "express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { User } from "./entities/User";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
+  //database connection
+  await createConnection({
+    type: "postgres",
+    database: "techchased",
+    logging: true,
+    synchronize: true,
+    entities: [User],
+  });
+
+  //create express session
   const app = express();
+
+  //cors
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+
+  //create Apollo Server instance
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+      validate: false,
+    }),
+  });
+
+  //apply apollo middleware to our app instance
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
     console.log("server started on  localhost:4000");
