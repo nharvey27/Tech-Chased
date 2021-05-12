9dfdabf1c4f3e4e71caa930dde431cd3de855d06
Nicholas Harvey <nharvey278@gmail.com>
2021-05-12 13:03:55 -0700

    create user field resolver to return user object associated with comment, created frontend createcomment mutation

--- server/src/resolvers/comment.ts
+++ server/src/resolvers/comment.ts
@@ -1,14 +1,29 @@
-import { Arg, Ctx, Int, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  FieldResolver,
+  Int,
+  Mutation,
+  Resolver,
+  Root,
+} from "type-graphql";
 import { Comment } from "src/entities/Comment";
 import { ObjectType } from "type-graphql";
 import { Entity, BaseEntity } from "typeorm";
 import { CommentInput } from "./commentInputs";
 import { MyContext } from "src/types";
+import { User } from "../entities/User";
 
 @ObjectType()
 @Entity()
 @Resolver(Comment)
 export class CommentResolver extends BaseEntity {
+  @FieldResolver(() => User)
+  async user(@Root() comment: Comment) {
+    const user = await User.findOne(comment.userId);
+
+    return user;
+  }
   @Mutation(() => Comment)
   async createComment(
     @Arg("options") options: CommentInput,
